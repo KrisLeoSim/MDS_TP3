bbd5106537892a63f4106393cdd6f75992d21a70
Kristhian <kris_leonardo94@hotmail.com>
2018-01-09 16:41:14 +0000

    bug Resposta correta e incorreta

--- quiz_controller.js
+++ quiz_controller.js
@@ -15,6 +15,10 @@ Quiz.prototype.isEnded = function(){
 Quiz.prototype.guess = function(answer){
     if(this.getQuestionIndex().correctAnswer(answer)){
         this.score++;
+        alert("Resposta Correta!");
+    }
+    else{
+        alert("Resposta Incorreta!");
     }
 
     this.questionIndex++;
