5a75eb5ab12dd8d87ecfa7760b3fd229ddf24d5b
Diogo Ameal <diogo_filipe111@hotmail.com>
2018-01-16 15:55:58 +0000

    Adding questions to Master Level

--- app_TestingIntegration.js
+++ app_TestingIntegration.js
@@ -55,9 +55,21 @@ function imprimeresm(){
 
 
 var questions = [
-    new Question("Testing + Integration Como se chama a mãe do kris ?",
-        ["El poderosa", "gutchigutchi", "el comandante", "Rosa a poderosa"],"Rosa a poderosa"),
-  
+    new Question("Quais são os 4 passos que constituem a fase de Continuous Integration? (atenção à ordem)",
+        ["Commit, Build, Testes Manuais, Implementação", "Commit, Build, Implementação, Testes Automáticos", "Commit, Build, Implementação, Testes Manuais", "Commit, Build, Testes Automáticos, Implementação"],
+        "Commit, Build, Testes Automáticos, Implementação"),
+    new Question("A criação dos testes automáticos permitem aos desenvolvedores analisarem os erros de integração. Uma boa prática é a utilização de frameworks XUnits. Quais das relações framework XUnit/linguagem de programação esta correta?",
+        ["PHPUnit / C++","CppUnit / C++","JUnit / PHP","CppUnit / Java"],
+        "CppUnit / C++"),
+    new Question("Os testes _____ concentram-se numa única “unidade de código”, geralmente uma função de um objecto ou módulo.",
+        ["de integração","funcionais","unitários","de aceitação"],
+        "unitários"),
+    new Question("Ao usar o BDD como cenário de teste, a equipa tem a possibilidade de manter a documentação viva, obter o feedback rápido e alinhar a comunicação com a equipa e com o cliente. Para escrever bons cenários, "
+        ["A equipa deve ser envolvida no processo","Devem descrever o comportamento passo a passo","Os cenários devem ser dependentes","Não se deve seguir a linguagem da história do utilizador"],
+        "A equipa deve ser envolvida no processo"),
+    new Question("TDD (Test Driven Development) é um desenvolvimento orientado por _____ , isto é, depois de efectuar a implementação da/do _____  cria-se a/o _____ .",
+        ["funcionalidades,teste,funcionalidade","testes,teste,funcionalidade","testes,funcionalidade,teste","funcionalidades,funcionalidade,teste"],
+        ""),
 ];
 
 var quizm = new QuizM(questions);
