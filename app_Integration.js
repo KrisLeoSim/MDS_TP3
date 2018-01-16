e3604a954aed29f636891eab4b9d393222dfd976
SCrolem <darkxield@gmail.com>
2018-01-16 01:59:46 +0000

    pontuação -  50% feito
    ja aparece a pontuação de cada

--- app_Integration.js
+++ app_Integration.js
@@ -1,14 +1,18 @@
 function populate(){
-    if(quiz.isEnded()){
-        showScores();
+    if(quizi.isEnded()){
+		
+		
+		localStorage.setItem('quizi', String(quizi.score));
+        location.href="Resultados.html";
+		
     }
     else{
         //show question
         var element = document.getElementById("question");
-        element.innerHTML=quiz.getQuestionIndex().text;
+        element.innerHTML=quizi.getQuestionIndex().text;
 
         // show choices
-        var choices = quiz.getQuestionIndex().choices;
+        var choices = quizi.getQuestionIndex().choices;
         for(var i = 0; i<choices.length; i++){
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
@@ -21,27 +25,36 @@ function populate(){
 function guess(id,guess) {
     var button = document.getElementById(id);
     button.onclick = function(){
-        quiz.guess(guess);
+        quizi.guess(guess);
         populate();
     }
 };
 
 function showProgress() {
-    var currentQuestionNumber = quiz.questionIndex + 1;
+    var currentQuestionNumber = quizi.questionIndex + 1;
     var element = document.getElementById("progress");
-    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quizi.questions.length;
 }
 
-function showScores() {
-    var gameOverHtml = "<h1>Result</h1>";
-    gameOverHtml += "<h2 id='score'>Your scores: " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHtml;
-};
-
 function imprimeresi(){
-    var element = document.getElementById("pintegration");
-    element.innerHTML = "" + quizi.score + "%";
+	
+	var element = document.getElementById("pintegration");
+	var valor = null;
+    
+	if (localStorage.getItem('quizi')) {
+        valor = localStorage.getItem('quizi');
+   
+
+    element.innerHTML = "" + valor + "%";
+   }else{
+	  
+    element.innerHTML = "" + "N/A"+ ""; 
+	   
+   }
+	
+	
+
+	
 };
 
 var questions = [
@@ -56,7 +69,7 @@ var questions = [
 ];
 
 
-var quiz = new QuizI(questions);
+var quizi = new QuizI(questions);
 
 
 
--- app_Testing.js
+++ app_Testing.js
@@ -1,15 +1,17 @@
 function populate(){
-    if(quiz.isEnded()){
+    if(quizt.isEnded()){
+        
+		localStorage.setItem('quizt', String(quizt.score));
         location.href="Resultados.html";
-        //showScores();
+     
     }
     else{
         //show question
         var element = document.getElementById("question");
-        element.innerHTML=quiz.getQuestionIndex().text;
+        element.innerHTML=quizt.getQuestionIndex().text;
 
         // show choices
-        var choices = quiz.getQuestionIndex().choices;
+        var choices = quizt.getQuestionIndex().choices;
         for(var i = 0; i<choices.length; i++){
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
@@ -22,31 +24,36 @@ function populate(){
 function guess(id,guess) {
     var button = document.getElementById(id);
     button.onclick = function(){
-        quiz.guess(guess);
+        quizt.guess(guess);
         populate();
     }
 };
 
 function showProgress() {
-    var currentQuestionNumber = quiz.questionIndex + 1;
+    var currentQuestionNumber = quizt.questionIndex + 1;
     var element = document.getElementById("progress");
-    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quizt.questions.length;
 }
 
-function showScores() {
-    var gameOverHtml = "<h1>Result</h1>";
-    gameOverHtml += "<h2 id='score'>Your scores: " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHtml;
-};
 
 function imprimerest(){
     var element = document.getElementById("ptesting");
-    element.innerHTML = "" + quiz.score + "%";
+    var valor = null;
+    
+	if (localStorage.getItem('quizt')) {
+        valor = localStorage.getItem('quizt');
+   
+
+    element.innerHTML = "" + valor + "%";
+   }else{
+	  
+    element.innerHTML = "" + "N/A"+ ""; 
+	   
+   }
 };
 
 var questions = [
-    new Question("Testing: que posição +e esta _/''''º ?",
+    new Question("Testing: que posição +e esta _/''''\º ?",
         ["de 2 ", "de 3", "de 4", "de 5"],"de 4"),
    
 ];
@@ -55,7 +62,7 @@ var questions = [
 
 
 
-var quiz = new QuizT(questions);
+var quizt = new QuizT(questions);
 
 
 
